a33e2f2ce6512dcebce2f1948ebde0e2b7536168
madhav1602 <madhavaggarwal580@gmail.com>
2025-05-01 13:58:57 +0530

    [Madhav] add: Add stock Api completed

--- src/main/java/com/cg/gotstock/controller/AuthController.java
+++ src/main/java/com/cg/gotstock/controller/AuthController.java
@@ -1,4 +1,26 @@
 package com.cg.gotstock.controller;
 
+
+import com.cg.gotstock.dto.UserRegisterDTO;
+import com.cg.gotstock.service.EmailService;
+import com.cg.gotstock.service.UserService;
+import jakarta.mail.MessagingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AuthController {
+    @Autowired
+     private UserService userService;
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping("/register-user")
+    public ResponseEntity<?> registerUser(@RequestBody UserRegisterDTO registerDTO) throws MessagingException {
+        emailService.sendEmail(registerDTO.getEmail(), "Registration successful", "Welcome to got-stock, your account has been created");
+        return userService.registerUser(registerDTO);
+    }
 }
--- src/main/java/com/cg/gotstock/service/StockAlertService.java
+++ src/main/java/com/cg/gotstock/service/StockAlertService.java
@@ -1,4 +1,7 @@
 package com.cg.gotstock.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class StockAlertService {
 }
