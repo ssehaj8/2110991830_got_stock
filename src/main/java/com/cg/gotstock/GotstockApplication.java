a167fe41d1b8ca24265709f007959bc4564f1f5b
madhav1602 <madhavaggarwal580@gmail.com>
2025-05-01 09:47:05 +0530

    [Madhav] add: initial commmit

---
+++ src/main/java/com/cg/gotstock/GotstockApplication.java
@@ -0,0 +1,13 @@
+package com.cg.gotstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotstockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cg/gotstock/GotstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.gotstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
